d7044e64bbd134eec7c2cf881b1f23d83c48c93e
Matthew-Noce <77691620+Matthew-Noce@users.noreply.github.com>
2021-10-22 10:23:38 -0400

    Update Arduino_code_.ino

--- IOT Lab/Arduino_code_/Arduino_code_.ino
+++ IOT Lab/Arduino_code_/Arduino_code_.ino
@@ -1,11 +1,11 @@
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-pinMode(7, OUTPUT);
 pinMode(2, OUTPUT);
 pinMode(3, OUTPUT);
 pinMode(4, OUTPUT);
 pinMode(5, OUTPUT);
+pinMode(6, OUTPUT);
 
 
 
